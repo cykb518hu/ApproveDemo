80007dc63074f3ef6dd82807fae8288c72f020ae
Achilles.Hu <Achilles.Hu@CHELPR-NW10002>
2017-06-05 18:36:52 +0800

    Add project files.

---
+++ FutureDemo/Controllers/DemoController.cs
@@ -0,0 +1,71 @@
+﻿using FutureDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FutureDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Page1()
+        {
+            return View();
+        }
+        public ActionResult Page2()
+        {
+            var dataList = DataLogic.ReadFromJsonFile();
+            return View(dataList);
+        }
+        public ActionResult Page3()
+        {
+            var dataList = DataLogic.ReadFromJsonFile();
+            return View(dataList);
+            return View();
+        }
+
+        public ActionResult AddNumber(string number)
+        {
+            float data = 0;
+            if (float.TryParse(number, out data))
+            {
+                var dataList = DataLogic.ReadFromJsonFile();
+                if (!dataList.Any(x => x.Data == data))
+                {
+                    dataList.Add(new UserData { Data = data, Display = false, AddDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") });
+                    DataLogic.SaveToJsonFile(dataList);
+                    return Json("Success", JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json("can't add dulicate data", JsonRequestBehavior.AllowGet);
+                }
+            }
+            return Json("you need to enter a number", JsonRequestBehavior.AllowGet);
+
+        }
+        public ActionResult Approve(string number)
+        {
+            float data = 0;
+            if (float.TryParse(number, out data))
+            {
+                var dataList = DataLogic.ReadFromJsonFile();
+                var item = dataList.FirstOrDefault(x => x.Data == data);
+                if (item != null)
+                {
+                    item.Display = true;
+                }
+                DataLogic.SaveToJsonFile(dataList);
+                return Json("Success", JsonRequestBehavior.AllowGet);
+            }
+            return Json("you need to enter a number", JsonRequestBehavior.AllowGet);
+
+        }
+
+
+    }
+
+
+}
---
+++ FutureDemo/Models/DataLogic.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace FutureDemo.Models
+{
+    public class DataLogic
+    {
+        public static List<UserData> ReadFromJsonFile()
+        {
+            List<UserData> dataList = new List<UserData>();
+            try
+            {
+                var fileName = HttpContext.Current.Server.MapPath("~/App_Data/Data.json");
+                var json = File.ReadAllText(fileName);
+                if (json.Length > 0)
+                {
+                    var jobj = JArray.Parse(json);
+                    dataList = JsonConvert.DeserializeObject<List<UserData>>(json);
+                }
+            }
+            finally
+            {
+
+            }
+            return dataList;
+        }
+        public static void SaveToJsonFile(List<UserData> dataList)
+        {
+            string json = JsonConvert.SerializeObject(dataList);
+
+            var fileName = HttpContext.Current.Server.MapPath("~/App_Data/Data.json");
+            System.IO.File.WriteAllText(fileName, json);
+        }
+    }
+
+    public class UserData
+    {
+        public float Data { get; set; }
+        public bool Display { get; set; }
+        public string AddDate { get; set; }
+    }
+}
